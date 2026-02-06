c2a429bbf3e284e35d9a860d99051ad5e08cc0e9
shifaakhan20 <shifaakhan860@gmail.com>
2026-02-06 21:15:12 +0530

    Enhance factorial program with user input and comments
    Updated coder name and date in comments, added input for number and calculation of factorial.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Shifaa
+# Date: 06/02/2026
 
 print("--- Factorial Finder ---\n")
 
-
 # Write your code here
+
+num = int(input("Enter Number: "))
+
+factorial = 1
+
+if num >= 0:
+    for i in range(1, num + 1):
+       factorial *= i
+       print(factorial)
+else:
+    print("Factorial of" , num , "is Not Defined")
+    
+
+
